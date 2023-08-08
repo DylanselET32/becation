8a83eb12b513be85c8caa4e51694e55968c8284a
nietonicolasleon <nietonicolasleon@gmail.com>
2023-08-08 15:16:54 -0300

    Se agregaron todos los métodos de UserModel

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -1,1 +1,31 @@
 /* Aquí los métodos para conseguir los datos de los usuarios en la BDD */
+const pool = require('../database/connection');
+const CRUD = require('../services/crud');
+
+/* ORDEN: TABLA, DATOS A PEDIR, EXTRAS*/
+
+/* GETS */
+const getAllUsers = async () => await CRUD.getAll('user', ["user_id", "name", "surname", "email", "dni", "is_able", "role_id", "available_days", "contrat_date"], "WHERE is_able = 1");
+
+const getUserById = async (id, extraClauses = "WHERE is_able = 1") => await CRUD.getById('user', id, ["user_id", "name", "surname", "email", "dni", "is_able", "role_id", "available_days", "contrat_date"],extraClauses);
+
+const getUserByColumn = async (column, value,extraClauses = "WHERE is_able = 1",fields = ["user_id", "name", "surname", "email", "dni", "is_able", "role_id", "available_days", "contrat_date"]) => await CRUD.getByColumn('user', column, value, fields, extraClauses);
+
+/* ADD */
+const addUser = async (data) => await CRUD.add('user', data);
+
+/* EDIT */
+const editUser = async (data, id) => await CRUD.edit('user', data, id);
+
+/* DELETE */
+const removeUser = async (id) => await CRUD.remove('user', id);
+
+
+module.exports = {
+    getAllUsers,
+    getUserById,
+    getUserByColumn,
+    addUser,
+    editUser,
+    removeUser,
+};
