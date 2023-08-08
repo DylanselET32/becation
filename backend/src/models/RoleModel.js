274b87d265a0c911eaacf08337433a76afc27920
nietonicolasleon <nietonicolasleon@gmail.com>
2023-08-08 15:31:23 -0300

    Se agregaron los métodos faltantes y el exports

--- backend/src/models/RoleModel.js
+++ backend/src/models/RoleModel.js
@@ -7,5 +7,23 @@ const getAllRoles = async () => await CRUD.getAll('role', ["role_id", "role_name
 
 const getRoleById = async (id) => await CRUD.getById('role', id, ["role_id", "role_name"]);
 
-const getRoleByColumn = async (column = "role_name", value, fields = ["role_id", "role_name"]) => await CRUD.getByColumn('role', column, value, fields);
+const getRoleByName = async (column = "role_name", value, fields = ["role_id", "role_name"]) => await CRUD.getByColumn('role', column, value, fields);
 
+/* ADD */
+const addRole = async (data) => await CRUD.add('role', data);
+
+/* EDIT */
+const editRole = async (data, id) => await CRUD.edit('role', data, id);
+
+/* DELETE */
+const removeRole = async (id) => await CRUD.remove('role', id);
+
+
+module.exports = {
+    getAllRoles,
+    getRoleById,
+    getRoleByName,
+    addRole,
+    editRole,
+    removeRole,
+};
