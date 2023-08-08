5003136cb7d7673742eddeb9d721f1096ced3f8a
Diego Sánchez Delgado <126181127+diegosanchezd@users.noreply.github.com>
2023-08-08 15:24:36 -0300

    header design

--- becation/src/components/Header.jsx
+++ becation/src/components/Header.jsx
@@ -15,6 +15,15 @@ const Header = () => {
                 <li className='li_route'><a href="#">Logout</a></li>
             </ul>
         </nav>
+        <nav className='menu_responsive'> 
+            <label className='burger_activator' htmlFor="check_menu"><span className="material-symbols-outlined logo_burger">menu</span></label>
+            <input type="checkbox" name="" id="check_menu" hidden/>
+            <ul className='routes_links_container_responsive' >
+                <li className='li_route'><a href="#">Login</a></li>
+                <li className='li_route'><a href="#">Menu</a></li>
+                <li className='li_route'><a href="#">Logout</a></li>
+            </ul>
+        </nav>
     </header>
   )
 }
