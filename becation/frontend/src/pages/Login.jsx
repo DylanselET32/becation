f70c4cf4a5df0af59cb1ab84806da9931e388fc7
Emiii12 <91755231+Emiii12@users.noreply.github.com>
2023-08-14 10:50:56 -0300

    Estilos Bootstrap
    Saqué algunas cosas que eran innecesarias porque no afectaban a nada.

--- becation/frontend/src/pages/Login.jsx
+++ becation/frontend/src/pages/Login.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 
 const styleCard = {
     borderRadius: "1rem",
+    background: "#2e2e2e"
 }
 
 const initalForm = {
@@ -36,19 +37,18 @@ export default function Login (){
     }
 
     return (
-        <section className="gradient-custom section_login">
-            <div className="container py-5 ">
+        <section className="section_login">
+            <div className="container py-5">
                 <div className="row d-flex justify-content-center align-items-center h-100 ">
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5 ">
-                        <form onSubmit={handleSubmit} className="card bg-dark text-white " style={styleCard}>
+                        <form onSubmit={handleSubmit} className="card text-white" style={styleCard}>
                             <div className="card-body p-5 text-center">
-                                <div className="mb-md-5 mt-md-4 pb-5">
-                                    <h2 className="fw-bold mb-2 text-uppercase">Iniciar Sesión</h2>
+                                <div className="mb-md-5 mt-md-4 pb-4">
+                                    <h2 className="fw-bold mb-5 text-uppercase">Iniciar Sesión</h2>
                                     <div className="form-outline form-white mb-4 input_container">
                                         <label className="form-label" htmlFor="typeEmail">Email</label>
                                         <div className="input_login">
                                             <input type="email" id="typeEmail" name="email" value={form.email} onChange={handleForm}/>
-                                            
                                         </div>
                                     </div>
                                     <div className="form-outline form-white mb-4 input_container">
