4662513d3fb7c7638528e6e4f8f4dd012302ad53
Emiii12 <91755231+Emiii12@users.noreply.github.com>
2023-08-23 17:28:48 -0300

    Login completo
    El login con las correcciones del responsive ya hechas.

--- becation/frontend/src/pages/Login.jsx
+++ becation/frontend/src/pages/Login.jsx
@@ -1,9 +1,13 @@
 import "../stylesheets/login.css"
+import "../stylesheets/modalAlert.css"
 import EyeToHide from "../imgs/eye-crossed.svg"
 import EyeHiden from "../imgs/eye.svg"
 import { useState } from "react"
+import ModalAlert from "../components/ModalAlert"
+import useModalAlert from "../helpers/useModalAlert"
+import { login } from "../services/userServices"
 
-const styleCard = {
+const styleContainer = {
     borderRadius: "1rem",
     background: "#2e2e2e"
 }
@@ -17,6 +21,8 @@ export default function Login (){
     
     const [passHidden, setPassHidden] = useState(false)
     const [form, setForm] = useState(initalForm)
+    const [ handleModalAlert, alert, openModalAlert, modalAlertCalled, msg, setMsg]= useModalAlert();
+
 
     const changePassVisibility = ()=>{
         setPassHidden(!passHidden)
@@ -29,45 +35,54 @@ export default function Login (){
         })
     }
 
-    const handleSubmit= ()=>{
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
         if(!form.email || !form.password){
-            window.alert("Dale gay")
-            return
+          openModalAlert();
+          setMsg("Complete los datos")
+          return
+        }
+        const [data, status]= await login(form)
+        if(status == 401){
+          openModalAviso();
+          setMsg("Contraseña Incorrecta")
+        }else if(status == 404){
+          openModalAviso();
+          setMsg("Usuario incorrecto")
+        }else if(status == 400){
+          openModalAviso();
+          setMsg("Nombre de usuario en uso")
+        }else if(status ==200){
+            console.log("LOGUEADO...")
+        //   redirec()
+        //   auth.reloaded()
         }
     }
 
     return (
-        <section className="section_login">
-            <div className="container py-5">
-                <div className="row d-flex justify-content-center align-items-center h-100 ">
-                    <div className="col-12 col-md-8 col-lg-6 col-xl-5 ">
-                        <form onSubmit={handleSubmit} className="card text-white" style={styleCard}>
-                            <div className="card-body p-5 text-center">
-                                <div className="mb-md-5 mt-md-4 pb-4">
-                                    <h2 className="fw-bold mb-5 text-uppercase">Iniciar Sesión</h2>
-                                    <div className="form-outline form-white mb-4 input_container">
-                                        <label className="form-label" htmlFor="typeEmail">Email</label>
-                                        <div className="input_login">
-                                            <input type="email" id="typeEmail" name="email" value={form.email} onChange={handleForm}/>
-                                        </div>
-                                    </div>
-                                    <div className="form-outline form-white mb-4 input_container">
-                                        <label className="form-label" htmlFor="typeContraseña">Contraseña</label>
-                                        <div className="input_login">
-                                            <input type={passHidden ? "text" : "password"} id="password" name="password" value={form.password} onChange={handleForm}/>
-                                            <img src={passHidden ? EyeHiden : EyeToHide} alt="" width={"30px"} onClick={changePassVisibility}/>
-                                        </div>
-                                    </div>
-                                    <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Olvidé mi Contraseña</a></p>
-                                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Continuar</button>
-                                </div>
-                            </div>
-                        </form>
+        <main>
+            <div className="contenedor" style={styleContainer}>
+                <form action="" onSubmit={handleSubmit} className="form" >
+                    <h2 className="form__title">Iniciá Sesión</h2>
+                    <div className="form__container">
+                        <div className="form__group">
+                            <label className="form__label" htmlFor="typeEmail">Email</label>
+                            <input type="email" className="form__input" name="email" value={form.email} onChange={handleForm}/>
+                        </div>
+                        <div className="form__group">
+                            <label className="form__label" htmlFor="typeContraseña">Contraseña</label>
+                            <input type={passHidden ? "text" : "password"} className="form__input" name="password" id="password" value={form.password} onChange={handleForm}/>
+                            <span className="form__span"><img src={passHidden ? EyeHiden : EyeToHide} alt="" width={"30px"} onClick={changePassVisibility}/></span>                  
+                        </div>
                     </div>
-                </div>
+                    <div className="register-link">
+                        <a href="#">Olvidé mi contraseña</a>
+                    </div>
+                    <button className="btn-login">Continuar</button>
+                </form>
             </div>
-            {/* <img src={EyeToHide} alt="" /> */}
-        </section>
+            {alert && <ModalAlert msg={msg} handleModalAlert={handleModalAlert} modalStyle={alert ? modalAlertCalled : "aviso-hidden"}/>}
+        </main>
     )
 }
     
