bfa086d0f88c6ea58cfa68f60e10bb9cb9cd0f9c
Diego Sánchez Delgado <126181127+diegosanchezd@users.noreply.github.com>
2023-08-23 16:43:33 -0300

    feat/calendar style changed

--- becation/frontend/src/pages/Calendar.jsx
+++ becation/frontend/src/pages/Calendar.jsx
@@ -3,9 +3,32 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import "../stylesheets/calendar.css"
+import interactionPlugin from '@fullcalendar/interaction';
 
+const events= [
+    { title: "Evento 3 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 4 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 5 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 6 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 7 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 8 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 9 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 10 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 11 ", start: "2023-08-23", end: "2023-08-30", },
+    { title: "Evento 12 ", start: "2023-08-23", end: "2023-08-30", }, { title: "Evento 5 ", start: "2023-08-23", end: "2023-08-30", },
+
+
+]
 
 export default function Calendar(){
+
+    const handleEventChange = (e)=>{
+        console.log(e.event._instance.range.start)
+    }
+
+    const handleEventClick = (e)=>{
+        console.log(e.event._def.title)
+    }
 
     return(
         <main>
@@ -13,11 +36,45 @@ export default function Calendar(){
         
                 <div className="calendar_container">
                     <FullCalendar
-                        plugins={[ dayGridPlugin ]}
+                        plugins={[ dayGridPlugin, interactionPlugin ]}
                         initialView="dayGridMonth"
+                        events={events}
+                        buttonText={{today: "Hoy"}}
+                        eventChange={handleEventChange}
+                        editable={true}
+                        eventClick={handleEventClick}
+                        dayMaxEventRows={true}
+                        height="700px"
+                        views= {{
+                            timeGridMonth: {
+                              dayMaxEventRows: 2 // adjust to 6 only for timeGridWeek/timeGridDay
+                            }
+                        }}
+                        eventResize={ function(info) {
+                            alert(info.event.title + " end is now " + info.event.end.toISOString());
+                        
+                            if (!confirm("is this okay?")) {
+                              info.revert();
+                            }
+                        }
+                    }
                     />
                 </div>
             </section>
+            <button>CREAR EVENTO</button>
         </main>
     )
+
+   
+    
 }
+// function renderEventContent(eventInfo) {
+//     return (
+//       <div className='custom-event'>
+//         <p className='event-title'>{eventInfo.event.title}</p>
+//       </div>
+//     );
+//   }
+
+
+// eventContent={true ? renderEventContent : null} para mas adelante por si sobre tiempo, callback para custom css
