fb07199f5b80e925068c3fd1b660151b948f0eb4
nietonicolasleon <nietonicolasleon@gmail.com>
2023-08-28 09:03:02 -0300

    Corregido el error de getVacationById

--- backend/src/controllers/vacationController.js
+++ backend/src/controllers/vacationController.js
@@ -56,7 +56,11 @@ const getVacationById = async (req, res) => {
   try {
     const id = req.params.id;
     const vacation = await vDAO.getVacationById(id);
-    res.status(200).json(vacation);
+    if (vacation == null) {
+      res.status(404).json({ message: "No se encontró el registro solicitado"}); 
+    }else{
+      res.status(200).json(vacation);
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
