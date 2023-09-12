f2b9a8734775293a25793f6861a0594c6b9f56b4
nietonicolasleon <nietonicolasleon@gmail.com>
2023-09-12 15:32:27 -0300

    Se creó los métodos get y add en VacationService

--- frontend/src/services/vacationService.js
+++ frontend/src/services/vacationService.js
@@ -2,6 +2,44 @@ import { setAuthToken, getAuthToken } from "../helpers/misc/authUtils";
 const apiUrl = import.meta.env.VITE_API_URL;
 const apiVersion = import.meta.env.VITE_API_VERSION;
 
+/* MÉTODOS GET */
+export async function getVacationById(id) {
+  try {
+      const response = await fetch(`${apiUrl}/${apiVersion}/area/getVacationById/${id}`, {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${getAuthToken()}`,
+          },
+      });
+      const data = await response.json();
+      const status = response.status;
+      if (status == 500) throw new Error('Error al conseguir la vacacion.'); 
+      return {data, status};
+  } catch (error) {
+      console.error(error);
+      throw new Error("Error al solicitar la vacacion");
+  }
+}
+
+export async function getVacationByColumn(nombreV){
+  try {
+      const response = await fetch(`${apiUrl}/${apiVersion}/area/getVacationByColumn/${nombreV}`, {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${getAuthToken()}`,
+          },
+      });
+      const data = await response.json();
+      const status = response.status;
+      if (status == 500) throw new Error('Error al conseguir la vacacion'); 
+      return {data, status};
+  } catch (error) {
+      console.error(error);
+      throw new Error("Error al solicitar la vacacion");
+  }
+}
 
 export async function getAllVacations() {
   try {
@@ -14,10 +52,108 @@ export async function getAllVacations() {
     });
     const data = await response.json();
     const status = response.status;
-    return {data:data, status};
+    if (status == 500) throw new Error('Error al conseguir las vacaciones.'); 
+    return {data, status};
   } catch (error) {
     console.error(error);
-    throw new Error("Error al solicitar usuario");
+    throw new Error("Error al solicitar la vacacion");
+  }
+}
+
+export async function getVacations() {
+  try {
+    const response = await fetch(`${apiUrl}/${apiVersion}/vacation/getVacations`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+      },
+    });
+    const data = await response.json();
+    const status = response.status;
+    if (status == 500) throw new Error('Error al conseguir las vacaciones.'); 
+    return {data, status};
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al solicitar la vacacion");
+  }
+}
+
+export async function getAllVacationsByArea() {
+  try {
+    const response = await fetch(`${apiUrl}/${apiVersion}/vacation/getAllVacationsByArea`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+      },
+    });
+    const data = await response.json();
+    const status = response.status;
+    if (status == 500) throw new Error('Error al conseguir las vacaciones.'); 
+    return {data, status};
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al solicitar la vacacion");
+  }
+}
+
+export async function getAllVacationsByUser() {
+  try {
+    const response = await fetch(`${apiUrl}/${apiVersion}/vacation/getAllVacationsByUser`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+      },
+    });
+    const data = await response.json();
+    const status = response.status;
+    if (status == 500) throw new Error('Error al conseguir las vacaciones.'); 
+    return {data, status};
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al solicitar la vacacion");
+  }
+}
+
+export async function getAllVacationsByEmployerId(id) {
+  try {
+    const response = await fetch(`${apiUrl}/${apiVersion}/vacation/getAllVacationsByEmployerId/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+      },
+    });
+    const data = await response.json();
+    const status = response.status;
+    if (status == 500) throw new Error('Error al conseguir las vacaciones.'); 
+    return {data, status};
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al solicitar la vacacion");
+  }
+}
+
+export async function getAllVacationsBetweenDates(start_date, end_date) {
+  try {
+    const obj = {startDate : start_date, endDate : end_date};
+    const response = await fetch(`${apiUrl}/${apiVersion}/vacation/getAllVacationsBetweenDays`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getAuthToken()}`,
+        body: JSON.stringify(obj),
+      },
+    });
+    const data = await response.json();
+    const status = response.status;
+    if (status == 500) throw new Error('Error al conseguir las vacaciones.'); 
+    return {data, status};
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error al solicitar la vacacion");
   }
 }
 
@@ -28,11 +164,11 @@ export async function addVacation(vacation) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(vacation),
     });
     const data = await response.json();
     const status = response.status;
-  
+    if (status == 500) throw new Error('Error al agregar el area'); 
     return {data, status};
   } catch (error) {
     console.error(error);
