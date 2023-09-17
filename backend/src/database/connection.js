126259cd6f4d95ee8747ec2b409101c0766a1f8b
dylanselET32 <dylan.seltzer.et32@gmail.com>
2023-09-16 22:58:31 -0300

    configuraciones para desployar

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'becation_db'
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE
 });
 
 module.exports = pool;
