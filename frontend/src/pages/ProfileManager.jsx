5106ecfd9c19667c5282918d66342df00c3ade20
GonzaGimenez <emaildegon@gmail.com>
2023-09-27 17:33:47 -0300

    Profile detail's modal created and running successfully

--- frontend/src/pages/ProfileManager.jsx
+++ frontend/src/pages/ProfileManager.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "../stylesheets/calendar.css";
-import FormVacation from '../components/FormVacation';
 import { addVacation, deleteVacation, getVacations } from '../services/vacationService';
 import { formatDateToString, operateDate } from '../helpers/misc/dateUtils';
 import { useAlert } from '../contexts/AlertContext'; // Importa el contexto
@@ -9,261 +7,120 @@ import CustomTable from '../components/CustomTable';
 import ModalEditVacation from '../components/vacationsModal/ModalSeeVacationDetaills';
 import { Modal } from 'react-bootstrap';
 import DeleteVacationBody from '../components/vacationsModal/DeleteVacationBody'
-import { getAllEmployers } from '../services/employeeServices';
+import { getAllEmployers, deleteEmployer } from '../services/employeeServices';
+import ModalSeeProfileDetails from '../components/profilesModal/ModalSeeProfileDetails';
+
 export default function Home({auth}){
 
-    const navigate = useNavigate();
-    const { alertConfig,setAlertConfig } = useAlert(); // Usa el contexto alert
-
-    const [isAvailableForm, setIsAvailableForm] = useState(false); // Estado que controla si el formulario de vacaciones está disponible
-    const [vacationDaysAsked, setVacationDaysAsked] = useState([{start: "", end: "", title: "Vacaciones"}]); // Estado que almacena los días de vacaciones solicitados
-    const [fetchDataToCalendar, setFetchDataToCalendara] = useState([]); // Estado que guarda los datos de vacaciones obtenidos del servidor
-    const [fetchData, setFetchData] = useState([]); // Estado que guarda los datos de vacaciones obtenidos del servidor
-    const [selectItem, setSelectItem] = useState(null); // Estado que almacena el elemento seleccionado en la tabla
-    const [actionButton, setActionButton] = useState(); // Estado que indica la acción a realizar
-    const [showModalSeeDetails,setShowModalSeeDetails] = useState(false); // Estado que controla al modal 
-    const [showModalDelete,setShowModalDelete] = useState(false); // Estado que controla al modal deletevacation
-
-    const toggleShowModalDelete = ()=>{setShowModalDelete(!showModalDelete)};
-    // Función para obtener las vacaciones del servidor
-    const fetchUsers = async () => {
-        try {
-          console.log("PRUEBAA")
-            setFetchData([])
-            const users = await getAllEmployers();
-            if(users.status !== 200) throw new Error("Error de servidor, intentar más tarde");
-            let temporalUsers = [];
-            users.data.map((event)=>{
-                const users = {
-                    allDay: true,
-                    color: "grey",
-                    editable: false,
-                    start: event.start_date,
-                    end: formatDateToString(operateDate(new Date(event.end_date), 1)),
-                    title: "Usuarios"
-                };
-                temporalUsers.push(users);
-            });
-            console.log("DATA:",users.data);
-            setFetchData(users.data);
-            setVacationDaysAsked(temporalUsers);
-            setFetchDataToCalendara(temporalUsers)
-        } catch (error) {
-            setAlertConfig({
-                show: true,
-                status: 'danger',
-                title: 'Error',
-                message: `Hubo un error al cargar las vacaciones, ${error}`,
-            });
-        }
-    };
-
-    // Función para manejar el evento de solicitud de formulario de vacaciones
-    const handleVacationFormRequest = () => {
-        if(isAvailableForm){
-            setVacationDaysAsked(fetchDataToCalendar)
-        }
-        setIsAvailableForm(!isAvailableForm);
-        
-    };
-
-    // Función para manejar el cambio en el evento del calendario
-    const handleEventChange = (e) => {
-        let initalYear= e.event._instance.range.start.getFullYear();
-        let initialMonth= (e.event._instance.range.start.getMonth()+1).toString();
-        let initialDay= operateDate(e.event._instance.range.start , 1).getDate();
-        const NEW_INITIAL_DATE= `${initalYear}-${initialMonth.length <= 1 ? `0${initialMonth}` : initialMonth}-${initialDay.length <= 1 ? `0${initialDay}` : initialDay}`;
-
-        let finalMonth= (e.event._instance.range.end.getMonth()+1).toString();
-        let finalDay= operateDate(e.event._instance.range.end , 1).getDate();
-
-        const NEW_END_DATE = `${initalYear}-${finalMonth.length <= 1 ? `0${finalMonth}` : finalMonth}-${finalDay.length <= 1 ? `0${finalDay}` : finalDay}`;
-
-        setVacationDaysAsked([...fetchDataToCalendar,
-            {
-                title: vacationDaysAsked[0].title,
-                start: NEW_INITIAL_DATE,
-                end:  formatDateToString(operateDate(new Date(NEW_END_DATE), 1)),
-            }
-        ]);
-    };
-
-    // Función para formatear los usuarios en la tabla
-    const formatUsersToTable = (users) => {
-        let temporalUsers = [];
-        const status = {
-            aproved: "Aprobado",
-            denied: "Denegado",
-            revision: "En Evaluación",
-            null: "Pendiente"
-        };
-    
-        // Ordena las vacaciones por la fecha de inicio más próxima
-        users.sort((a, b) => new Date(b.start_date) - new Date(a.start_date));
-        
-        users.forEach((event) => {
-            const user = {
-                ...event,
-                start_date: formatDateToString(new Date(event.start_date), "DD/MM/YYYY"),
-                end_date: formatDateToString(new Date(event.end_date), "DD/MM/YYYY"),
-                date_asked: formatDateToString(new Date(event.date_asked), "DD/MM/YYYY hh:mm:ss"),
-                status: status[event.status]
-            };
-            temporalUsers.push(vacation);
-        });
-    
-        return temporalUsers;
-    };
-    
-
-    // Función para manejar el evento de clic en un evento del calendario
-    const handleEventClick = (e) => {
-
-    };
-
-    // Función para manejar el envío del formulario de vacaciones
-    const handleForm = (state) => {
-        console.log(fetchData)
-        setVacationDaysAsked([...fetchDataToCalendar,
-            {
-                title: 'Usuario',
-                start: state.initialDate,
-                end: formatDateToString(operateDate(new Date(state.finalDate), 2))
-            }
-        ]);
-    };
-
-    // Función para manejar la presentación del formulario
-    const handleSubmit = async e => {
-        e.preventDefault();
-        let vacationToSend = vacationDaysAsked[vacationDaysAsked.length -1];
-        vacationToSend = {
-            start_date: new Date(vacationToSend.start),
-            end_date:  operateDate(new Date(vacationToSend.end), -1),
-            status: null,
-            note: null,
-            date_asked: new Date(),
-            area_manager_authorization: null,
-        };
-        try {
-            await addVacation(vacationToSend);
-            setAlertConfig({
-                show: true,
-                status: 'success',
-                title: 'Guardado',
-                message: 'Se creó la vacación exitosamente',
-            });
-            refresh()
-            setIsAvailableForm(false);
-        } catch (error) {
-            console.error(error.message);
-            setAlertConfig({
-                show: true,
-                status: 'danger',
-                title: 'Error',
-                message: `No se pudo registrar la fecha, inténtelo nuevamente más tarde (${error.message})`,
-            });
-        }
-    };
-
-
-
-    // Función para manejar las acciones de la tabla
-    const handleAction = (e) => {
-        switch (e.type) {}
-        useEffect(() => {},handleAction,)
-        name
-    };
-    // Función que se llama en la tabla para condicional las acciones
-    const isDisabledCondition = (row,child) =>{
-        // row es la fila a analisar y child es el boton de accion que estan abajo
-        return(row['area_manager_authorization'] !== null && child.props.name != 'seeDetails')
+  const navigate = useNavigate();
+  const { alertConfig,setAlertConfig } = useAlert(); // Usa el contexto alert
+  const [isAvailableForm, setIsAvailableForm] = useState(false); // Estado que controla si el formulario de empleados está disponible
+  const [fetchData, setFetchData] = useState([]); // Estado que guarda los datos de empleados obtenidos del servidor
+  const [selectItem, setSelectItem] = useState(null); // Estado que almacena el elemento seleccionado en la tabla
+  const [actionButton, setActionButton] = useState(); // Estado que indica la acción a realizar
+  const [showModalSeeDetails,setShowModalSeeDetails] = useState(false); // Estado que controla al modal 
+  const [showModalDelete,setShowModalDelete] = useState(false); // Estado que controla al modal deletevacation
+  const toggleShowModalDelete = ()=>{setShowModalDelete(!showModalDelete)};
+  
+  // Función para obtener los empleados del servidor
+  const fetchEmployers = async () => {
+    try {
+      setFetchData([])
+      const employers = await getAllEmployers();
+      if(employers.status !== 200) throw new Error("Error de servidor, intentar más tarde");
+      setFetchData(employers.data);
+    }catch (error) {
+      setAlertConfig({
+        show: true,
+        status: 'danger',
+        title: 'Error',
+        message: `Hubo un error al cargar los empleados, ${error}`,
+      });
     }
-
-    // Efecto para realizar una acción según el botón presionado
-    useEffect((e) => {
-        switch (actionButton){
-            case "edit":
-                hanleEditVacation(selectItem);
-                break;
-            case "delete":
-                hanleDeleteVacation(selectItem);
-                break;
-            case "seeDetails":
-                hanleSeeDetailsVacation(selectItem);
-                break;
-
-        }
-    },[selectItem,actionButton]);
-
-    // Función para manejar la edición de una vacación
-    const hanleEditVacation = (item) => {
-        console.log("se esta editando",item);
-    };
-
-    // Función para manejar la eliminación de una vacación
-    const hanleDeleteVacation = async (item) => {
-        
-            console.log("Se esta eliminando",item);
-            toggleShowModalDelete()
-    };
-    
-
-    // Función para manejar la visualización de detalles de una vacación
-    const hanleSeeDetailsVacation = (item) => {
-        console.log("se esta Viendo detalles",item);
-        setShowModalSeeDetails(true)
-    };
-    const refresh = () => {
-        fetchUsers();
-      };
-    // Efecto para cargar las vacaciones al montar el componente
-    useEffect(()=>{
-        fetchUsers();
-    },[]);
-
-    return(
-        <div className="container-lg">
-            <Modal show={showModalDelete} onHide={toggleShowModalDelete}>
-                  <DeleteVacationBody
-                    title="Eliminar Vacacion"
-                    refresh={refresh}
-                    toggle={toggleShowModalDelete}
-                    item={selectItem}
-                    itemView=""
-                    delete={deleteVacation}
-                  />
-            </Modal>
-            <ModalEditVacation show={showModalSeeDetails} setShow={setShowModalSeeDetails} item={selectItem}/>
-            <div className="row">
-                <section className='d-flex flex-column mt-3 col-lg-6 col-md-12 col-10 text-center mx-auto d-flex'>                    
-                    <button  className={isAvailableForm ? "button_ask_vacation_called" : "button_ask_vacation"} onClick={handleVacationFormRequest}>
-                        {isAvailableForm ? "Cancelar" : "Pedir Vacaciones"}
-                    </button>
-                    <div className='form_vacation_container'>
-                        <div className='d-flex justify-content-center text-align-center w-100'>
-                        <FormVacation isCalled={isAvailableForm} formFather={handleForm} handleSubmit={handleSubmit}/>
-                        </div>
-                    </div>  
-                        <CustomTable
-                            rows={fetchData.filter(v=>v.is_able==true)}
-                            fields={[
-                                // ["date_asked","Solicitud"],
-                                ["name","Nombre"],
-                                ["email","Email"],
-                                ["dni","DNI"]
-                            ]}
-                            setSelectItem={setSelectItem}
-                            msgNotRows="No hay vacaciones pendientes"
-                            isDisabledCondition={isDisabledCondition}
-                            maxHeight={!isAvailableForm?"75vh":'48vh'}
-                        > 
-                            <button className="btn p-0 btn_table w-100" name='edit' onClick={()=>{setActionButton("edit")}}>Editar <i className="bi bi-pencil-square"></i></button>
-                            <button className="btn p-0 btn_table w-100" name='delete' onClick={()=>{setActionButton("delete")}}>Eliminar <i className="bi bi-calendar-x-fill"></i></button>
-                            <button className="btn p-0 btn_table w-100" name='seeDetails' onClick={()=>{setActionButton("seeDetails")}}>Ver Detalles <i className="bi bi-eye"></i></button>
-                        </CustomTable>
-                </section>
-            </div>
-        </div>
-    );
+  };
+  // Función para manejar las acciones de la tabla
+  const handleAction = (e) => {
+    switch (e.type) {}
+    useEffect(() => {},handleAction,)
+    name
+  };
+  // Función que se llama en la tabla para condicional las acciones
+  const isDisabledCondition = (row,child) =>{
+    // row es la fila a analizar y child es el boton de accion que estan abajo
+    return(row['area_manager_authorization'] !== null && child.props.name != 'seeDetails')
+  }
+  // Efecto para realizar una acción según el botón presionado
+  useEffect((e) => {
+    switch (actionButton){
+      case "edit":
+        handleEditEmployer(selectItem);
+        break;
+      case "delete":
+        handleDeleteEmployer(selectItem);
+        break;
+      case "seeDetails":
+        handleSeeDetailsEmployer(selectItem);
+        break;
+    }
+    setActionButton("")
+  },[selectItem,actionButton]);
+  // Función para manejar la edición de un empleado
+  const handleEditEmployer = (item) => {
+    console.log("se esta editando",item);
+  };
+  // Función para manejar la eliminación de un empleado
+  const handleDeleteEmployer = async (item) => {
+    console.log("Se esta eliminando",item);
+    toggleShowModalDelete()
+  };
+  // Función para manejar la visualización de detalles de un empleado
+  const handleSeeDetailsEmployer = (item) => {
+    console.log("se esta viendo detalles",item);
+    setShowModalSeeDetails(true)
+  };
+  const refresh = () => {
+    fetchEmployers();
+  };
+  // Efecto para cargar los empleados al montar el componente
+  useEffect(()=>{
+    fetchEmployers();
+  },[]);
+  return(
+    <div className="container-lg">
+      <h1>{actionButton}</h1>
+      <Modal show={showModalDelete} onHide={toggleShowModalDelete}>
+        <DeleteVacationBody
+          title="Eliminar Vacacion"
+          refresh={refresh}
+          toggle={toggleShowModalDelete}
+          item={selectItem}
+          itemView=""
+          delete={deleteVacation}
+        />
+      </Modal>
+      <ModalSeeProfileDetails show={showModalSeeDetails} setShow={setShowModalSeeDetails} item={selectItem}/>
+      <div className="row">
+        <section className='d-flex flex-column mt-3 col-lg-6 col-md-12 col-10 text-center mx-auto d-flex'>
+          <CustomTable
+            rows={fetchData.filter(v=>v.is_able==true)}
+            fields={[
+              // ["date_asked","Solicitud"],
+              ["name","Nombre"],
+              ["email","Email"],
+              ["area","Area"],
+              ["role_name","Rol"]
+            ]}
+            setSelectItem={setSelectItem}
+            msgNotRows="No se encontraron empleados"
+            isDisabledCondition={isDisabledCondition}
+            maxHeight={!isAvailableForm?"75vh":'48vh'}
+          > 
+            <button className="btn p-0 btn_table w-100" name='edit' onClick={()=>{setActionButton("edit")}}>Editar <i className="bi bi-pencil-square"></i></button>
+            <button className="btn p-0 btn_table w-100" name='delete' onClick={()=>{setActionButton("delete")}}>Eliminar <i className="bi bi-calendar-x-fill"></i></button>
+            <button className="btn p-0 btn_table w-100" name='seeDetails' onClick={()=>{setActionButton("seeDetails")}}>Ver Detalles <i className="bi bi-eye"></i></button>
+          </CustomTable>
+        </section>
+      </div>
+    </div>
+  );
 }
