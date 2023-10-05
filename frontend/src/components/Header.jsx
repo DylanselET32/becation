c79928631a460b6f2451f6cf1cf93d97a4510013
dylanselET32 <dylan.seltzer.et32@gmail.com>
2023-10-05 11:10:32 -0300

    debug header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import LogoBecation from "../imgs/BeCation_logo.png"
 import "../stylesheets/header.css"
 import ConfirmationModal from './ConfirmationModal';
 import useConfirmation from '../hooks/useConfirmation';
+import { Link } from 'react-router-dom';
 //import ReturnLinks from './HeaderVariants';
 const Header = ({auth}) => {
 
@@ -28,12 +29,12 @@ return (<>
         </div>
         <nav className='nav_container'>
             <ul className='routes_links_container'>
-                <li className='li_route'><a href="home">Home</a></li>
+                <li><Link className='li_route' to="/">Home</Link></li>
+                {user?.privileges >= 2 ?  <li><Link className='li_route' to="/areaBoss">Jefe del area</Link></li> : ""}
+                {user?.privileges >= 3 ? <li><Link className='li_route' to="/vacationManager">Admin. Vacaciones</Link></li> : ""}
+                {user?.privileges >= 3 ? <li><Link className='li_route' to="/profileManager">Admin. de Perfiles</Link></li> : ""}
+                {user?.privileges >= 3 ? <li><Link className='li_route' to="/adminAreaRole">Admin. Area-Rol</Link></li> : ""}
                 {!user ? <li className='li_route'><a href="login">Login</a></li> : <li className='li_route'><a onClick={singOut}>Logout</a></li>}
-                {user?.privileges >= 2 ? <li className='li_route'><a href="areaBoss">Jefe del area</a></li> : ""}
-                {user?.privileges >= 3 ? <li className='li_route'><a href="vacationManager">Admin. Vacaciones</a></li> : ""}
-                {user?.privileges >= 3 ? <li className='li_route'><a href="profileManager">Admin. de Perfiles</a></li> : ""}
-                {user?.privileges >= 3 ? <li className='li_route'><a href="adminAreaRole">Admin. Area-Rol</a></li> : ""}
                 {user != null ? <li className='li_route user_name'>{user.name}</li> : ""}
             </ul>
         </nav>
@@ -44,10 +45,10 @@ return (<>
                 <li className='li_route'><a href="home">Home</a></li>
                 <li className='li_route'><a href="login">Login</a></li>
                 <li className='li_route'><a onClick={singOut}>Logout</a></li>
-                {user?.privileges >= 2 ? <li className='li_route'><a href="areaBoss">Jefe del area</a></li> : ""}
-                {user?.privileges >= 3 ? <li className='li_route'><a href="vacationManager">Admin. Vacaciones</a></li> : ""}
-                {user?.privileges >= 3 ? <li className='li_route'><a href="profileManager">Admin. de Perfiles</a></li> : ""}
-                {user?.privileges >= 3 ? <li className='li_route'><a href="adminAreaRole">Admin. Area-Rol</a></li> : ""}
+                {user?.privileges >= 2 ?  <li><Link className='li_route' to="/areaBoss">Jefe del area</Link></li> : ""}
+                {user?.privileges >= 3 ? <li><Link className='li_route' to="/vacationManager">Admin. Vacaciones</Link></li> : ""}
+                {user?.privileges >= 3 ? <li><Link className='li_route' to="/profileManager">Admin. de Perfiles</Link></li> : ""}
+                {user?.privileges >= 3 ? <li><Link className='li_route' to="/adminAreaRole">Admin. Area-Rol</Link></li> : ""}
             </ul>
         </nav>
     </header>
